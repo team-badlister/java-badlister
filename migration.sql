d5c74a1f42f595ebe182989eb1867aef84fefc6f
Aaron Trevino <Aaron@Aarons-MacBook-Pro.local>
2018-08-28 16:37:22 -0500

    edited migration file

--- migration.sql
+++ migration.sql
@@ -43,6 +43,7 @@ CREATE TABLE pads (
   id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
   user_id     INT UNSIGNED NOT NULL,
   title       VARCHAR(240) NOT NULL,
+  location    VARCHAR(240),
   description TEXT         NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (user_id) REFERENCES users (id)
