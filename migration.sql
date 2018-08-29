aa5f05e7dcbd22b4a37dc35faf6cdac8337ab805
Aaron Trevino <Aaron@aarons-mbp.attlocal.net>
2018-08-28 21:47:26 -0500

    adding ads to database is working

--- migration.sql
+++ migration.sql
@@ -1,26 +1,3 @@
-# USE adlister_db;
-#
-# DROP TABLE IF EXISTS ads;
-# DROP TABLE IF EXISTS users;
-#
-# CREATE TABLE users (
-#     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-#     username VARCHAR(240) NOT NULL,
-#     email VARCHAR(240) NOT NULL,
-#     password VARCHAR(255) NOT NULL,
-#     PRIMARY KEY (id)
-# );
-#
-# CREATE TABLE ads (
-#     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-#     user_id INT UNSIGNED NOT NULL,
-#     title VARCHAR(240) NOT NULL,
-#     description TEXT NOT NULL,
-#     PRIMARY KEY (id),
-#     FOREIGN KEY (user_id) REFERENCES users(id)
-#         ON DELETE CASCADE
-# );
-
 show databases;
 
 
@@ -39,7 +16,7 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE pads (
+CREATE TABLE ads (
   id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
   user_id     INT UNSIGNED NOT NULL,
   title       VARCHAR(240) NOT NULL,
